9e9e0e6797aaa097182b38f72d12412379144d74
Remedy Online <agyemangmichael555@gmail.com>
2024-12-21 00:09:20 +0000

    changed large images to smaller sized images for faster page loading...

--- src/constants/testimonials.js
+++ src/constants/testimonials.js
@@ -17,19 +17,19 @@ const testimonials = [
 	},
 	{
 		id: "3",
-		img: "satisfiedClients/client3.jpg",
-		rating: "4",
-		comment:
-			"Very professional approach to our project. He took time to understand our requirements and delivered a user-friendly interface that our staff loves using.",
-		clientName: "Martin Asiedu Amoako",
-	},
-	{
-		id: "4",
 		img: "satisfiedClients/client2.jpg",
 		rating: "5",
 		comment:
 			"The website functionality was good, though we needed a few revisions to get the design exactly right. Communication was clear throughout the project.",
 		clientName: "Emily Adjei",
+	},
+	{
+		id: "4",
+		img: "satisfiedClients/client3.jpg",
+		rating: "4",
+		comment:
+			"Very professional approach to our project. He took time to understand our requirements and delivered a user-friendly interface that our staff loves using.",
+		clientName: "Martin Asiedu Amoako",
 	},
 ];
 
